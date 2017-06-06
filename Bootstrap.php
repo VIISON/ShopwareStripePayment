c52367acdf291f56010e62ce926bbb21786492f1
Mihai Plasoianu <mihai.plasoianu@viison.com>
2017-06-06 18:11:22 +0200

    Add CSRFWhitelistAware dummy for Shopware versions prior to 5.2

--- Bootstrap.php
+++ Bootstrap.php
@@ -3,6 +3,8 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once(__DIR__ . '/vendor/autoload.php');
 }
 
+require_once(__DIR__ . '/Components/CSRFWhitelistAware.php');
+
 use Shopware\Models\Config\Element;
 use Shopware\Plugins\StripePayment\Classes\SmartyPlugins;
 use Shopware\Plugins\StripePayment\Subscriber;
